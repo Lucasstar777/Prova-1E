6fac79bae4d413d247bbff8ee626344c5a86ad2a
Lucas Costa <lucas.cruz.campos@escola.pr.gov.br>
2025-08-06 08:32:22 -0300

    Delete Lucas Costa Prova 1E directory

--- Lucas Costa Prova 1E/CalculadoraMaluca.py
+++
@@ -1,9 +0,0 @@
-print ("Lucas Costa Cruz Campos")
-print ("Prova de introdução a programação")
-
-#recebe os numeros
-num1 = float (input("digite o primeiro número: "))
-num2 = float (input("digite o segundo número: "))
-
-#pega os numeros e multiplica
-print("O resultado da multiplicação é:", num1 * num2)
